3d4e99d2651a47cb53e7c7cc65eb925b99953184
SunHailang <hai-lang.sun@ubisoft.com>
2019-09-04 19:14:59 +0800

    update

--- Utils/TimeUtils.py
+++ Utils/TimeUtils.py
@@ -8,11 +8,75 @@
 
 import time, math
 import json
+import datetime
+
+import calendar
+
+import os
+
+
+class UTC(datetime.tzinfo):
+    def __init__(self, offset = 0):
+        self._offset = offset
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(hours=self._offset)
+    
+    def tzname(self, dt):
+        return 'UTC +%s' % self._offset
+    
+    def dst(self, dt):
+        return datetime.timedelta(hours=self._offset)
 
 # return 时间戳 和 本地时间(yyyy-MM-dd HH:mm:ss)
-def GetCurrentTime():
+def GetLocalTime():
     ticks = math.floor(time.time())
     localTime = time.strftime('%Y-%m-%d %H:%M:%S')
     return ticks, localTime
 
+def GetUtcTime():
+    ticks = math.floor(datetime.datetime.utcnow().timestamp())
+    utcTime = datetime.datetime.fromtimestamp(ticks)
+    return ticks, utcTime
     
+def timestampTodatetime(stamp):
+    localTicks = datetime.datetime.fromtimestamp(stamp)
+    utcTicks = datetime.datetime.utcfromtimestamp(stamp)
+    return localTicks, utcTicks
+
+def getYearOfDay(dt):
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    totalday = 0
+    if calendar.isleap(dt.year):
+        days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for month in range(1, dt.month):
+        totalday  = totalday + days[month-1]
+    totalday = totalday + dt.day
+    return totalday
+
+
+if __name__ == "__main__":
+    print('date.max:', datetime.date.max)
+    print('date.min:', datetime.date.min)
+    print('date.today():', datetime.date.today())
+    print('date.fromtimestamp():', datetime.date.fromtimestamp(time.time()))
+
+    utcnow = datetime.datetime.utcnow()
+    
+    print('utc time: ', datetime.datetime.utcnow())
+    print('local time: ',datetime.datetime.now().timestamp())
+    #北京时间
+    beijing = datetime.datetime(utcnow.year, utcnow.month, utcnow.day, utcnow.hour, utcnow.minute, utcnow.second,tzinfo = UTC(8))
+    print(UTC(8).tzname(beijing))
+    print('beijing: {}'.format(beijing))
+
+    utcTicks, uctTime = GetUtcTime()
+
+    print(utcTicks, uctTime)
+    print('year:{}, month:{}, day:{}, hour:{}, minute:{}, second:{}, weedday:{}, yearday:{}'.format(uctTime.year, uctTime.month, uctTime.day, uctTime.hour, uctTime.minute, uctTime.second, uctTime.weekday(), getYearOfDay(uctTime)))
+    ts = '1569772800'
+    tc = eval(ts)
+    print(timestampTodatetime(tc))
+    print('******************************************')
+    print(time.localtime(time.time()).tm_yday)
+    print(getYearOfDay(datetime.datetime.now()))
