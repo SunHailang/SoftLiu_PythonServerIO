aa535d31cf26dc4a7bf4e9000c39d7c02a298a02
SunHailang <shl971277663@outlook.com>
2019-09-20 11:00:16 -0700

    update query train url"
    ;

--- RequestUtil/LeftqueryUtil.py
+++ RequestUtil/LeftqueryUtil.py
@@ -48,6 +48,7 @@ class LeftqueryUtil(object):
         print('from station name: {} -> {}'.format(form_station, fromstation))
         tostation = station_name[to_station]
         print('to station name: {} -> {}'.format(to_station, tostation))
+        # https://kyfw.12306.cn/otn/leftTicket/queryA?leftTicketDTO.train_date=2019-09-20&leftTicketDTO.from_station=UUH&leftTicketDTO.to_station=SHH&purpose_codes=ADULT
         url = 'https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.form_station={}&leftTicketDTO.to_station={}&purpose_code=ADULT'.format(
             date, fromstation, tostation
         )
